f385f934796083fae83885e12f10d2f04a8dd81e
Trinité Antoine <antoine.trinite@gmail.com>
2023-04-02 14:13:54 +0200

    Ajout des boutons dans le script terminé, création des catégories terminée, en cours d'ajout du tri visuel en fonction des catégories

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,30 +1,53 @@
-// création des constantes pour le html
 const gallery = document.querySelector('.gallery')
+const btnGroup = document.querySelector('#category-group');
 
 // récupération des informations depuis le fichier JSON
 fetch('http://localhost:5678/api/works')
 .then((response) => response.json())
 .then(data => {
-    data.forEach(works => {
+
+    //affichage de l'image et des titres
+    data.forEach(function(works) {
         const fig = document.createElement('figure')
         const titre = document.createElement('figcaption')
         const img = document.createElement('img')
 
-        // récupération de l'image et du titre
+        // récupération de l'image, du titre et des catégories.
         const imageURL = works.imageUrl;
         img.src = imageURL;
         titre.innerText = works.title;
         let category = works.category.name;
-        console.log(category)
+
         // ajout du code à l'HTML
         fig.appendChild(img);
         fig.appendChild(titre);
         gallery.appendChild(fig);
     });
+    // Tableau set
+    const setOfCategories = new Set();
+
+    // création du bouton "tous"
+    const tousBtn = document.createElement('button');
+        tousBtn.innerText = "Tous";
+        tousBtn.classList.add('category-btn');
+        btnGroup.appendChild(tousBtn);
+
+    // première itération pour ajouter les catégories uniques
+    data.forEach(function(works) {
+        setOfCategories.add(works.category.name);
+    });
+    // deuxième itération pour créer les boutons des catégories
+    setOfCategories.forEach(function(category) {
+        let btn = document.createElement('button');
+        btn.innerText = category;
+        btn.classList.add('category-btn');
+        btnGroup.appendChild(btn);
+    });
+
 })
 .catch(error => {
     console.error(error);
-  });
+});
 
 
 
