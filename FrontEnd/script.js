ed1713dda66252d39fd7cadd22eb656077de0421
Trinité Antoine <antoine.trinite@gmail.com>
2023-04-17 13:26:43 +0200

    ajout de la base de la modale, activable et désactivable

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,8 +1,7 @@
 const gallery = document.querySelector('.gallery')
 const btnGroup = document.querySelector('#category-group');
 let data = null
-const logoutBtn = document.getElementById('log');
-const projects = document.getElementById('projects');
+
 // récupération des informations depuis le fichier JSON
 fetch('http://localhost:5678/api/works')
 .then((response) => response.json())
@@ -84,36 +83,134 @@ setOfCategories.forEach(function(category) {
     });
 };
 
-// Récupération du token de connexion
-const token = sessionStorage.getItem('token');
-console.log(token)
+//création du bouton logout
+const logoutBtn = document.getElementById('log');
 
-// Option de déconnexion
-if (token) {
-    logoutBtn.innerText = 'Logout';
-} else {
-    logoutBtn.innerText = 'Login'
-}
-logoutBtn.addEventListener('click', () => {
-    sessionStorage.clear();
-})
-
-//création du bouton modifier pour la fenêtre modale
-const modifyBtn = document.createElement("button");
+//creation des boutons de modif
+let modifyBtn;
+function createButton(targetElement, paddingLeft, paddingTop) {
+    const modifyBtn = document.createElement("button");
     modifyBtn.textContent = 'modifier';
     modifyBtn.style.backgroundColor = 'transparent';
     modifyBtn.style.border = 'none';
     modifyBtn.style.color = 'black';
     modifyBtn.style.fontSize = '14px';
-const modifyIcon = document.createElement("i");
+    modifyBtn.style.cursor = 'pointer';
+  
+    const modifyIcon = document.createElement("i");
     modifyIcon.className = "fa-regular fa-pen-to-square";
     modifyIcon.style.fontSize = '14px';
     modifyIcon.style.color = 'black';
-    modifyIcon.style.paddingLeft = '31px';
-
+    modifyIcon.style.paddingLeft = paddingLeft + 'px';
+    modifyIcon.style.paddingTop = paddingTop + 'px';
     modifyIcon.appendChild(modifyBtn);
-    projects.appendChild(modifyIcon);
+    targetElement.appendChild(modifyIcon);
+  
+    return modifyBtn;
+  }
+  
+  const projects = document.getElementById('projects');
+  const profile = document.getElementById('profile');
 
-modifyBtn.addEventListener('click', (event) => {
+
+// Récupération du token de connexion
+const token = sessionStorage.getItem('token');
+console.log(token)
+
+// déclaration des boutons avant pour qu'ils soient accessible en dehors de la boucle
+  let profilModifBtn;
+  let projectsModifBtn;
+// Option de déconnexion
+if (token) {
+    //logout option
+    logoutBtn.innerText = 'Logout';
+
+    //boutons de modification
+    profilModifBtn = createButton(profile, 60, 13);
+    projectsModifBtn = createButton(projects, 30, 0);
+} else {
+    logoutBtn.innerText = 'Login'
+}
+
+logoutBtn.addEventListener('click', () => {
+    sessionStorage.clear();
+})
     
-  });
+
+// création de la modale - PARTIE 1
+//bg
+ const modalBg = document.createElement('div');
+ modalBg.style.position = 'fixed';
+ modalBg.style.top = '0';
+ modalBg.style.left = '0';
+ modalBg.style.width = '100%';
+ modalBg.style.height = '100%';
+ modalBg.style.background = 'rgba(0, 0, 0, 0.3)';
+ modalBg.style.display = "none";
+ document.body.appendChild(modalBg);
+
+//boite
+const modalBox = document.createElement('div');
+modalBox.style.position = 'fixed';
+modalBox.style.top = '50%';
+modalBox.style.left = '50%';
+modalBox.style.transform = 'translate(-50%, -50%)';
+modalBox.style.width = '630px';
+modalBox.style.height = '731px';
+modalBox.style.background = '#ffff';
+modalBox.style.borderRadius = '10px'
+modalBg.appendChild(modalBox)
+//bouton close
+const closeBtn = document.createElement('button');
+closeBtn.style.position = 'fixed';
+closeBtn.style.top = '26px';
+closeBtn.style.right = '30px';
+closeBtn.style.height = '24px';
+closeBtn.style.width = '24px';
+closeBtn.style.backgroundColor = '#ffff';
+closeBtn.style.border = 'none';
+closeBtn.className = "fa-solid fa-xmark";
+closeBtn.style.fontSize = '24px';
+closeBtn.style.textAlign = 'center';
+closeBtn.style.cursor = 'pointer';
+modalBox.appendChild(closeBtn);
+//gallerie avec photos éditables
+
+//trait
+
+//bouton ajouter une photo
+
+//bouton supprimer gallerie
+
+
+// ouverture et fermeture de la modale
+    projectsModifBtn.onclick = function () {
+        modalBg.style.display = "block";
+    }
+
+    // Close the modal
+    closeBtn.onclick = function () {
+        modalBg.style.display = "none";
+    }
+
+// Suppression des travaux existants - PARTIE 2
+//     function delRequest() {
+//      fetch('https://api.example.com/data', {
+//     method: 'DELETE', 
+//     headers: {
+//         'Content-Type': 'application/json',
+//         'Authorization': 'token'
+//     },
+// })
+//     .then((response) => response.json())
+//     .then(response2 => {
+//         data = response2
+//     })
+//     .catch(error => {
+//         console.error(error);
+//     });   
+//     }
+
+// Envoi d’un nouveau projet au back-end via le formulaire de la modale - PARTIE 3
+
+// Traitement de la réponse de l’API pour afficher dynamiquement la nouvelle image de la modale.
